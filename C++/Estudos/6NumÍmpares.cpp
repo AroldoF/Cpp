2e0df77428b9eea7983b0a38ad0ee7228a4de24b
Arôldo Fernandes <aroldo.caval@gmail.com>
2024-04-27 17:59:48 -0300

    soma dos numeros impares

--- C++/Estudos/6NumÍmpares.cpp
+++ C++/Estudos/6NumÍmpares.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 int main()
 {
-    int n, i,z=0;
-    cout<<"Digite um numero!"<<endl;
-    cin>> n;
+    int n, i, z = 0;
+    cout << "Digite um numero!" << endl;
+    cin >> n;
     int vet[6];
-    i=n;
+    i = n;
     do
     {
-        if (i%2!=0&&i!=n&&i!=n+1)
+        if (i % 2 != 0 && i != n)
         {
-            vet[z]=i;
+            vet[z] = i;
             z++;
         }
         i++;
-    }while(z<6);
-    cout<<"Os numeros impares depois do numero "<<n<<" serao os numeros:"<<endl;
-    for (i = 0; i <6; i++)
+    } while (z < 6);
+    cout << "Os numeros impares depois do numero " << n << " serao os numeros:" << endl;
+    for (i = 0; i < 6; i++)
     {
-        cout<<vet[i]<<" ";
+        cout << vet[i] << " ";
     }
-    
+
     return 0;
 }
